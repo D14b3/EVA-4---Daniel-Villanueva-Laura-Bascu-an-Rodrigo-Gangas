d43e9d32e66ea3247adf9b2534cff0141d4cb7f6
rxdrxgo <holamellamorodrigoxde@gmail.com>
2024-06-30 23:45:39 -0400

    Se inicia el desarrollo de la funcionalidad de editar personaje

--- personaje.py
+++ personaje.py
@@ -12,7 +12,7 @@ class Personaje():
     def mysql(self):
         return self.__mysql
 
-    def crearPj(self)-> None:
+    def crearPj(self) -> None:
         nombre = input('Indique el nombre del personaje: ')
         raza = input(f'Indique la raza de {nombre}: ')
         nivel = 1
@@ -23,5 +23,16 @@ class Personaje():
         self.mysql.connection.commit()
         sql = "SELECT nombrePersonaje, raza, nivel, estado FROM `personaje` WHERE `nombrePersonaje`=%s"
         self.mysql.cursor.execute(sql, (nombre))
+        result = self.mysql.cursor.fetchone()
+        print(result)
+
+
+    def editarPj(self) -> None:
+        id = int(input('Indique el id del personaje a modificar'))
+        sql = "UPDATE"
+        self.mysql.cursos.execute(sql,())
+        self.mysql.connection.commit()
+        sql = "SELECT"
+        self.mysql.cursor.execute(sql, ())
         result = self.mysql.cursor.fetchone()
         print(result)
