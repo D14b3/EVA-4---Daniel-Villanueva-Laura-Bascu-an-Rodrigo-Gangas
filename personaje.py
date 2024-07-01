49efa53ca69447336d825cb8b363e7883ca9716b
rxdrxgo <holamellamorodrigoxde@gmail.com>
2024-06-30 22:42:05 -0400

    Se implementa la funcion de agregar personaje a la clase Personaje

--- personaje.py
+++ personaje.py
@@ -1,28 +1,28 @@
 import pymysql.cursors
+import pymysql
 
-def CrearPj():
-    nombre = input('Indique el nombre del personaje')
-    raza = input(f'Indique la raza de {nombre}')
-    nivel = 1
-    estado = 'vivo'
-    
-    connection = pymysql.connect(host='localhost',
-                                 user='user',
-                                 password='passwd',
-                                 database='db',
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
-    
-    with connection:
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO `personaje` (`nombrePersonaje`, `raza`, `nivel`, `estado`) VALUES (%s, %s, %s, %s)"
-            cursor.execute(sql, (nombre, raza, nivel, estado))
-            
-            connection.commit()
-            
+
+class Personaje():
+
+    def crearPj():
+        nombre = input('Indique el nombre del personaje: ')
+        raza = input(f'Indique la raza de {nombre}: ')
+        nivel = 1
+        estado = 'vivo'
+        connection = pymysql.connect(host='localhost',
+                                     user='root',
+                                     password='',
+                                     database='rol',
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
+        with connection:
             with connection.cursor() as cursor:
-                
-                sql = "SELECT * FROM `personaje` WHERE `nombrePersonaje`=%s"
-                cursor.execute(sql, (nombre))
-                result = cursor.fetchone()
-                print(result)
+                sql = "INSERT INTO `personaje` (`nombrePersonaje`, `raza`, `nivel`, `estado`) VALUES (%s, %s, %s, %s)"
+                cursor.execute(sql, (nombre, raza, nivel, estado))
+                connection.commit()
+                with connection.cursor() as cursor:
+                    
+                    sql = "SELECT * FROM `personaje` WHERE `nombrePersonaje`=%s"
+                    cursor.execute(sql, (nombre))
+                    result = cursor.fetchone()
+                    print(result)
