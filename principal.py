c9923d374cda8e2d9b62b74e43946197e08506d2
rxdrxgo <holamellamorodrigoxde@gmail.com>
2024-07-03 22:08:36 -0400

    Se agregan las nuevas funciones al archivo principal

--- principal.py
+++ principal.py
@@ -1,8 +1,18 @@
 from conexion import Conexion
 from personaje import Personaje
 from arma import Arma
+from raza import Raza
+from habilidad import Habilidad
+
 Conexion.getConnection()
 
+Habilidad().crearHabilidad()
+Habilidad().crearHabilidad()
+Raza().crearRaza()
 Personaje().crearPj()
 Arma().crearArma()
-Personaje().agregarArmaPj()
+
+
+Habilidad().elegirHabilidad()
+Raza().elegirRaza()
+Personaje().elegirArma()
