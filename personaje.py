05a4f845e3d4a641fd47c77995bdc1d681f5d971
rxdrxgo <holamellamorodrigoxde@gmail.com>
2024-07-03 22:07:16 -0400

    Se modifica el archivo personaje segun las nuevas funcionalidad

--- personaje.py
+++ personaje.py
@@ -15,21 +15,21 @@ class Personaje():
     def crearPj(self) -> None:
         system('cls')
         nombre = input('Indique el nombre del personaje: ')
-        raza = input(f'Indique la raza de {nombre}: ')
+  
         nivel = 1
         estado = 'vivo'
 
-        sql = "INSERT INTO `personaje` (`nombrePersonaje`, `raza`, `nivel`, `estado`) VALUES (%s, %s, %s, %s)"
-        self.mysql.cursor.execute(sql, (nombre, raza, nivel, estado))
+        sql = "INSERT INTO `personaje` (`nombrePersonaje`, `nivel`, `estado`) VALUES (%s, %s, %s)"
+        self.mysql.cursor.execute(sql, (nombre, nivel, estado))
         self.mysql.connection.commit()
-        sql = "SELECT idPersonaje, nombrePersonaje, raza, nivel, estado FROM `personaje` WHERE `nombrePersonaje`=%s"
+        sql = "SELECT idPersonaje, nombrePersonaje, nivel, estado FROM `personaje` WHERE `nombrePersonaje`=%s"
         self.mysql.cursor.execute(sql, (nombre))
         result = self.mysql.cursor.fetchone()
         print(result)
         time.sleep(5)
 
 
-    def agregarArmaPj(self) -> None:
+    def elegirArma(self) -> None:
         system('cls')
         id = input('Indique el id del personaje a modificar: ')
         idarma = input('Indique el id del arma a agregar: ')
@@ -38,7 +38,7 @@ class Personaje():
         self.mysql.connection.commit()
         registrosModificados = self.mysql.cursor.rowcount
         print(f'Registros modificados: {registrosModificados}')
-        sql = "SELECT nombrePersonaje, raza, nivel, estado, idArma FROM `personaje` WHERE `idPersonaje`=%s"
+        sql = "SELECT nombrePersonaje, nivel, estado, idArma, idRaza FROM `personaje` WHERE `idPersonaje`=%s"
         self.mysql.cursor.execute(sql, (int(id)))
         result = self.mysql.cursor.fetchone()
         print(result)
