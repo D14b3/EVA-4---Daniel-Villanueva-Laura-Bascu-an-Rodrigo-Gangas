1399c92d3c4b61f3c31aa0c75bb4d9728ed93f22
Laura Bascuñán L <108833125+Bascunanlau@users.noreply.github.com>
2024-07-08 16:04:40 -0400

    Update mostrPersonaje.py
    Crear opcion para generar tabla que muestre a los personajes que se puede escojer

--- mostrPersonaje.py
+++ mostrPersonaje.py
@@ -1,34 +1,31 @@
+from os import system
 import beautifultable as bs
+
 
 # Lista vacía para almacenar los personajes
 personajes = []
 
-# Función para agregar personajes a la lista
-def agregar_personaje():
-    nombre = input("Ingrese el nombre del personaje: ")
-    descripcion = input("Ingrese la descripción del personaje: ")
-    personaje = {"nombre": nombre, "descripcion": descripcion}
-    personajes.append(personaje)
-
 # Función para generar la tabla
-def generar_tabla(personajes):
+def generar_tabla(self) -> None:
+    system("cls")
+    sql = "SELECT nombre, FROM personaje"
+    self.mysql.cursor.execute(sql)
+    resultados = self.mysql.cursor.fetchall()
     table = bs.BeautifulTable()
-    table.columns.header = ["Nombre", "Descripción"]
-    for personaje in personajes:
-        table.rows.append([personaje["nombre"], personaje["descripcion"]])
-    return table
+    table.columns.header = ["Nombre",]
+    for resultado in resultados:
+        table.rows.append([resultado[0]])
+    print(table)
 
 # Creamos un menú para elegir la opción de agregar personajes o generar la tabla
 while True:
     print("Menú:")
-    print("1. Agregar personaje")
     print("2. Generar tabla de personajes")
     print("3. Salir")
     opcion = input("Elija una opción: ")
     
-    if opcion == "1":
-        agregar_personaje()
-    elif opcion == "2":
+
+    if opcion == "2":
         if len(personajes) == 0:
             print("No hay personajes agregados. Agregue personajes primero.")
         else:
