e977512cd27591ad4c56f0a82991d044bf55527c
Laura Bascuñán L <108833125+Bascunanlau@users.noreply.github.com>
2024-07-08 22:02:16 -0400

    Update mostrPersonaje.py

--- mostrPersonaje.py
+++ mostrPersonaje.py
@@ -1,12 +1,24 @@
+import pymysql
+from conexion import Conexion 
 from os import system
+import time
 import beautifultable as bs
 
 
 # Lista vacía para almacenar los personajes
 personajes = []
 
+class listaPersonaje():
+    def __init__(self) -> None:
+        Conexion.getConnection()
+        self.__mysql = Conexion()
+
+    @property
+    def mysql(self):
+        return self.__mysql
+
 # Función para generar la tabla
-def generar_tabla(self) -> None:
+def crearTabla(self) -> None:
     system("cls")
     sql = "SELECT nombre, FROM personaje"
     self.mysql.cursor.execute(sql)
@@ -16,20 +28,20 @@ def generar_tabla(self) -> None:
     for resultado in resultados:
         table.rows.append([resultado[0]])
     print(table)
+    time.sleep(5)
 
-# Creamos un menú para elegir la opción de agregar personajes o generar la tabla
+# Menú para elegir la opción de agregar personajes o generar la tabla
 while True:
     print("Menú:")
     print("2. Generar tabla de personajes")
     print("3. Salir")
     opcion = input("Elija una opción: ")
     
-
     if opcion == "2":
         if len(personajes) == 0:
             print("No hay personajes agregados. Agregue personajes primero.")
         else:
-            tabla = generar_tabla(personajes)
+            tabla = crearTabla(personajes)
             print(tabla)
     elif opcion == "3":
         break
